0def53c51805dc1cb96fd55f7c0ff20b69ef9c6b
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-09 18:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,5 +29,8 @@ console.log (kittens);
  }
  
  function removeLastKitten(){
-   return kittens.slice (kittens.length-2)
+   return kittens.slice (0,kittens.length-1);
+ }
+ function removeFirstKitten(){
+   return kittens.slice(1);
  }
